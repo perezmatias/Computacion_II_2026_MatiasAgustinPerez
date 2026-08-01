0495c2d60e5340c023840dce8a7b063667d5d6e5
perezmatias <maag.perez@alumno.um.edu.ar>
2026-08-01 14:27:15 -0300

    feat: main integra Manager dict y levanta analizador de memoria

--- Tp1/src/main.py
+++ Tp1/src/main.py
@@ -1,11 +1,24 @@
 import multiprocessing
 import time
 from recolector import iniciar_recolector
+# Importamos la función de nuestro nuevo analizador
+from analizadores.memoria import iniciar_analizador_memoria
 
 def main():
     print("Iniciando Monitor de Procesos...")
 
-    # Creamos un diccionario con una cola independiente para cada analizador
+    # --- MEMORIA COMPARTIDA ---
+    # Creamos el Manager. Esto levanta un proceso "servidor" invisible
+    manager = multiprocessing.Manager()
+    
+    # Este es nuestro "pizarrón global" seguro para multiprocesos
+    snapshot_global = manager.dict()
+    
+    # Inicializamos la clave de memoria vacía para que la interfaz gráfica
+    # no tire error si intenta leer antes de que el analizador escriba algo.
+    snapshot_global['memoria'] = {}
+
+    # --- COMUNICACIÓN (COLAS) ---
     colas_analizadores = {
         'resumen': multiprocessing.Queue(),
         'memoria': multiprocessing.Queue(),
@@ -16,25 +29,37 @@ def main():
         'sistema': multiprocessing.Queue()
     }
 
-    # Definimos el proceso Recolector y le pasamos todas las colas
+    # --- LEVANTAMOS LOS PROCESOS ---
+    # 1. El Recolector (le pasamos todas las colas)
     proceso_recolector = multiprocessing.Process(
         target=iniciar_recolector, 
         args=(colas_analizadores,)
     )
-
-    # Iniciamos el recolector
     proceso_recolector.start()
 
+    # 2. El Analizador de Memoria (le pasamos SU cola y el pizarrón global)
+    proceso_memoria = multiprocessing.Process(
+        target=iniciar_analizador_memoria,
+        args=(colas_analizadores['memoria'], snapshot_global)
+    )
+    proceso_memoria.start()
+
     try:
-        # Loop temporal para probar
+        # Loop temporal para verificar que el snapshot se está llenando
         while True:
-            time.sleep(2)
+            # Leemos directamente del snapshot global
+            diccionario_memoria = snapshot_global['memoria']
+            print(f"[MAIN] Snapshot global actualizado: {len(diccionario_memoria)} procesos en memoria.")
+            time.sleep(3)
             
     except KeyboardInterrupt:
         print("\nApagando monitor...")
     finally:
+        # Limpieza de procesos al salir con Ctrl+C
         proceso_recolector.terminate()
+        proceso_memoria.terminate()
         proceso_recolector.join()
+        proceso_memoria.join()
         print("Monitor apagado correctamente.")
 
 if __name__ == "__main__":
